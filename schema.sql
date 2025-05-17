60efa1d5b091b9b643b63264b5d0ab20cc22dd58
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2025-05-17 19:53:13 +0300

    quick fix

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS Player (
 
 CREATE TABLE IF NOT EXISTS Game (
   game_id SERIAL PRIMARY KEY,
-  num_players INT NOT NULL;
+  num_players INT NOT NULL,
   pid0 INT REFERENCES Player(pid),
   pid1 INT REFERENCES Player(pid),
   pid2 INT REFERENCES Player(pid),
